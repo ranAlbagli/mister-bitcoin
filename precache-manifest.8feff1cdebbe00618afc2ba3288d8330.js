5e6bc4024f99c4d453c1aa0d342e1b8e222df028
Ran  albagli <albagli.ran@gmail.com>
2019-10-08 11:25:42 +0300

    Updates

---
+++ precache-manifest.8feff1cdebbe00618afc2ba3288d8330.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0180ef70eb6fcf002cc8291a5023804d",
+    "url": "/ranAlbagli/mister-bitcoin.git/index.html"
+  },
+  {
+    "revision": "af79a23aa1cdcc96192a",
+    "url": "/ranAlbagli/mister-bitcoin.git/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "64b5dd69c7236649862d",
+    "url": "/ranAlbagli/mister-bitcoin.git/static/css/main.8afd5e83.chunk.css"
+  },
+  {
+    "revision": "af79a23aa1cdcc96192a",
+    "url": "/ranAlbagli/mister-bitcoin.git/static/js/2.7d90e54c.chunk.js"
+  },
+  {
+    "revision": "64b5dd69c7236649862d",
+    "url": "/ranAlbagli/mister-bitcoin.git/static/js/main.3400b582.chunk.js"
+  },
+  {
+    "revision": "158eb203d88ce4f4e027",
+    "url": "/ranAlbagli/mister-bitcoin.git/static/js/runtime-main.88816073.js"
+  },
+  {
+    "revision": "1d898d9fa45250b05adeff0007fe6f14",
+    "url": "/ranAlbagli/mister-bitcoin.git/static/media/logo.1d898d9f.svg"
+  }
+]);
